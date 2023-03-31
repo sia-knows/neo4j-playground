9c4d7b2e45d076af3e027c2f6fe7273f5e2968bd
gagocarrilloedgar <edgargc.upc@gmail.com>
2023-03-31 14:10:51 +0200

    chore(typescript): add pool size and connection config options

--- typescript/src/neo4j/Neo4jConnection.ts
+++ typescript/src/neo4j/Neo4jConnection.ts
@@ -5,7 +5,11 @@ export class Neo4jConnection {
 
   static create(uri: string, user: string, password: string): Session {
     if (!this.driver) {
-      this.driver = neo4j.driver(uri, neo4j.auth.basic(user, password));
+      this.driver = neo4j.driver(uri, neo4j.auth.basic(user, password), {
+        maxConnectionPoolSize: 100,
+        maxConnectionLifetime: 30 * 60,
+        connectionAcquisitionTimeout: 2 * 60
+      });
     }
     return this.driver.session();
   }
